d2e700cbe85fece718176eaf9b8b62c32f26dcab
Vadim Volkovskiy <vadim_1423@mail.ru>
2018-11-29 03:09:57 +0300

    Создание каркаса

---
+++ script.js
@@ -0,0 +1,40 @@
+(function(){
+	'use strict';
+	
+	//Опишем наши игровые объекты + научим их рисовать себя на канвасе и передвигаться
+	var Ball = function () {
+		return {
+			
+		}
+	};
+
+	//Блоки для отбивания шарика
+	var Bracket = function () {
+		return {
+			
+		}
+	};
+
+	//Собственно сам игрок с его свойствами
+	var Player = function () {
+		return {
+			
+		};
+	};
+
+	//Теперь сама игра
+	var Game = function () {
+
+		return this;
+	};
+
+	//В прототип будем писать методы всякие игровые
+	Game.prototype = {
+
+	};
+
+	//При загрузке window, стартуем нашу игру
+	window.onload = function () {
+        //Game Start!!
+	}		
+})();
