f8585db87164222ddefc4035fbc8f5ee8d96079b
Vadim Volkovskiy <vadim_1423@mail.ru>
2018-11-30 06:02:04 +0300

    startGame 2

--- script.js
+++ script.js
@@ -137,7 +137,7 @@
 			this.objects.bracket1.x = 50;
 			this.objects.bracket1.y = this.params.height / 2 + this.objects.bracket1.h / 2;
 			
-			this.objects.bracket2.x = this.params.width + 50;
+			this.objects.bracket2.x = this.params.width - 50;
 			this.objects.bracket2.y = this.params.height / 2 - this.objects.bracket1.h / 2;
 
 			//Перекрасим второго игрока
